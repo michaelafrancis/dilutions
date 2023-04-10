47cabb316f8aa12b02c6f0cda848f05c825152b2
michaelafrancis <michaela.francis@icloud.com>
2023-04-10 14:10:25 -0700

    updated script

---
+++ dilutions.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import csv
+
+# Get user input for dilution factor, number of replicates, and final volume of replicates
+assay = input("Enter the assay, Y42 or Y77: ")
+date = input("Enter the date in DDMMMYYYY: ")
+coral_id = input("Enter the Coral ID: ")
+dilution_factor = input("Enter dilution factor: ")
+vol_sample = input("Enter Volume of Sample, usually use 10-15uL: ")
+dilution_factor = int(dilution_factor)*2
+num_replicates = input("Enter number of final replicates: ")
+final_volume = input("Enter final volume of replicates (in uL): ")
+total_volume = 600
+
+
+
+# Create dilution scheme list
+step1_dilution_factor = int(total_volume)/int(vol_sample)
+dilution_scheme = [(int(vol_sample), 600 - int(vol_sample), "1/{}: {}uL sample + {}uL a.d.".format(step1_dilution_factor, int(vol_sample), 600 - int(vol_sample)))]
+
+
+# Step 2 Dilution  
+step2_dilution_factor = (int(dilution_factor)/int(step1_dilution_factor))/2
+print(F"step2 dilution factor = {step2_dilution_factor}, {dilution_factor}/{step1_dilution_factor}")
+
+vol_prev_dilution = int(total_volume)/int(step2_dilution_factor)
+print(F" vol_prev_dilution = {vol_prev_dilution}, {total_volume}/{step2_dilution_factor}")
+
+vol_diluent = int(total_volume) - int(vol_prev_dilution)
+print(F"vol_diluent = {vol_diluent}, {total_volume, vol_prev_dilution}")
+
+instruction = "1/{}: {}uL prev. dilution + {}uL a.d.".format(step2_dilution_factor, int(total_volume)/int(step2_dilution_factor), int(total_volume) - int(vol_prev_dilution))
+dilution_scheme.append((vol_prev_dilution, vol_diluent, instruction))
+
+
+# Add final dilution step with replicates
+dilution_factor *= 2
+vol_prev_dilution = 300
+vol_diluent = 300
+instruction = "1/2 Serial Dilution: {}uL prev. dilution + {}uL a.d.".format(vol_prev_dilution, vol_diluent)
+dilution_scheme.append((vol_prev_dilution, vol_diluent, instruction))
+
+final_instruction = "replicates = {}, {}uL each".format(num_replicates, final_volume)
+dilution_scheme.append(("N/A", final_volume, final_instruction))
+
+# Write dilution scheme to CSV file
+with open(F'coral{coral_id}_{assay}_dilution_scheme_{date}.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Step #", "Instructions", "Dilution Factor"])
+    for i, step in enumerate(dilution_scheme):
+        # writer.writerow(["Step {}".format(i+1), step[2], step[0] / int(step[1])])
+            if i == 0:
+                writer.writerow(["Step {}".format(i+1), step[2], step1_dilution_factor])
+            elif i == 1:
+                writer.writerow(["Step {}".format(i+1), step[2], step1_dilution_factor * step2_dilution_factor])
+            elif i == 2:
+                writer.writerow(["Step {}".format(i+1), step[2], 2 * step1_dilution_factor * step2_dilution_factor])
+            else:
+                writer.writerow(["Step {}".format(i+1), step[2], 2 * step1_dilution_factor * step2_dilution_factor])
+
